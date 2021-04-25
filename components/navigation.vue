32a0cdcca327041a6a85cb755b9de8c0c6414688
jorisheijkant <jorisheijkant@gmail.com>
2021-04-25 21:10:18 +0200

    Added progress bar, changed typography

--- components/navigation.vue
+++ components/navigation.vue
@@ -12,6 +12,9 @@
                     <img class="burger-icon" src="../assets/img/burger.png" alt="">
                 </div>
             </div>
+            <div class="progress-bar">
+                <div class="bar" :style="{'width': progress}"></div>
+            </div>
         </div>
         <div class="menu" :class="{'open': menu}">
             <h2 class="menu__heading">Snel naar</h2>
@@ -33,8 +36,13 @@ export default {
 
     data() {
         return {
-            menu: false
+            menu: false,
+            progress: "1%"
         }
+    },
+
+    mounted() {
+        window.addEventListener("scroll", this.updateProgressIndicator);
     },
 
     methods: {
@@ -45,6 +53,13 @@ export default {
         goTo(item) {
             this.menu = false;
             this.$scrollTo(`.${item}`);
+        },
+
+        updateProgressIndicator() {
+            const { documentElement, body } = document;
+            let windowScroll = body.scrollTop || documentElement.scrollTop;
+            let height = documentElement.scrollHeight - documentElement.clientHeight;
+            this.progress = (windowScroll / height) * 100 + "%";
         }
     },
 
